5a6cf719334de529fa7affca5f1ef9e61454fe81
Josh Kerrigan <joshua_kerrigan@alumni.brown.edu>
2020-09-21 23:42:41 -0400

    test.py error fixed

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ def test_mask():
     assert sum(mask) == 10
 
 def test_data_prep():
-    df = pd.DataFrame({'domain':['test.domain1','test.domain2 test.domain3'],'blocked_chain':['1','0 1'],'mask_count'=[1,2]})
+    df = pd.DataFrame({'domain':['test.domain1','test.domain2 test.domain3'],'blocked_chain':['1','0 1'],'mask_count':[1,2]})
     tok_model = fn.load_sentpiece()
     tok_result = fn.prep_data(df,tokenizer=tok_model)
     assert len(tok_result) > 0
