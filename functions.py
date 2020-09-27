10bbcce0cfd4c97a34661a014ca64ad135503918
Josh <josh@Joshs-MacBook-Pro.local>
2020-09-27 11:46:32 -0400

    New metric learning model created.

--- functions.py
+++ functions.py
@@ -67,41 +67,52 @@ def load_yttm():
     return bpe
         
 def parse_data(df):
-    format_df = df.groupby('timestamp').sum().reset_index()
-    format_df['domain'] = df.groupby('timestamp')['domain'].apply(lambda x: '|'.join(x)).reset_index()['domain']
-    format_df['domain_list'] = df.groupby('timestamp')['domain'].apply(lambda x: ','.join(x)).reset_index()['domain']
-    format_df['mask_count'] = df.groupby('timestamp')['domain'].apply(lambda x: len(x)).reset_index()['domain']
+    df_ = df.copy()
+    df_['domain'] = df_['domain'].apply(lambda x: x.replace('.',' ').replace('-',' '))
+#    format_df = df.groupby('timestamp').sum().reset_index()
+#    format_df['domain'] = df.groupby('timestamp')['domain'].apply(lambda x: '|'.join(x)).reset_index()['domain']
+#    format_df['domain_list'] = df.groupby('timestamp')['domain'].apply(lambda x: ','.join(x)).reset_index()['domain']
+#    format_df['mask_count'] = df.groupby('timestamp')['domain'].apply(lambda x: len(x)).reset_index()['domain']
     #format_df['domain'] = format_df['domain'].apply(lambda x: ' '.join(x.split('.')))
-    format_df['blocked_chain'] = df.groupby('timestamp')['blocked'].apply(lambda x: ' '.join([str(i) for i in x])).reset_index()['blocked']
-    return format_df
+#    format_df['blocked_chain'] = df.groupby('timestamp')['blocked'].apply(lambda x: ' '.join([str(i) for i in x])).reset_index()['blocked']
+    return df_['domain']
 
-def prep_data(df,tokenizer=None):
+def prep_data(df,timestamps,tokenizer=None):
     print(df.domain)
-    encoded_docs = tokenizer.encode(list(df.domain.values), output_type=yttm.OutputType.ID) #change null space to |
-    encoded_docs = pad_sequences(encoded_docs,100,padding='post')
-    masks = make_mask(df.mask_count.values[0]).reshape(1,-1)
-    labels = make_multilabel(df.blocked_chain.values[0]).reshape(1,-1)
-    return encoded_docs,masks,labels
+    encoded_queries = pad_sequences(tokenizer.encode(list(df.loc[df.timestamp>timestamps[1]].domain.values), output_type=yttm.OutputType.ID),50,padding='post') #change null space to |
+    df_neg = df.loc[(df.blocked==0)&(df.timestamp<timestamps[1])].domain.values
+    df_neg = df_neg[:len(encoded_queries)]
+    df_anchors = df.loc[(df.blocked==1)&(df.timestamp<timestamps[1])].domain.values
+    df_anchors = df_anchors[:len(encoded_queries)]
+    encoded_neg = pad_sequences(tokenizer.encode(list(df_neg), output_type=yttm.OutputType.ID),50,padding='post')
+    encoded_anchors = pad_sequences(tokenizer.encode(list(df_pos), output_type=yttm.OutputType.ID),50,padding='post')
+    return encoded_queries,encoded_neg,encoded_anchors
 
 def run_all(tokenizer=None,timestamp=None):
     if not tokenizer:
         tokenizer = load_tokenizer()
-    dframe = load_query_list(timestamp=timestamp)
+    buffer_timestamp = str(int(timestamp)-1000)
+    dframe = load_query_list(timestamp=buffer_timestamp)
+    new_entries = np.sum(dframe.timestamp>=timestamp)
     i = 0
-    while len(dframe) < 1:
+    while len(new_entries) < 1:
         if i == 0:
             print('Awaiting additional matter...')
         time.sleep(2)
-        dframe = load_query_list(timestamp=timestamp)
+        dframe = load_query_list(timestamp=buffer_timestamp)
+        new_entries = np.sum(dframe.timestamp>=timestamp)
         i+=1
     most_recent_timestamp = dframe.timestamp.iloc[-1]
-    dframe['timestamp'] = dframe.timestamp.round(-1)
-    parsed_dframe = parse_data(dframe).iloc[-1] # pull a single timestamp
-    token_data,mask_data,labels = prep_data(parsed_dframe,tokenizer=tokenizer)
-    return token_data,mask_data,labels,parsed_dframe,most_recent_timestamp
+#    dframe['timestamp'] = dframe.timestamp.round(-1)
+#    parsed_dframe = parse_data(dframe).iloc[-1] # pull a single timestamp
+    parsed_dframe = parse_data(dframe)
+    token_queries,token_neg,token_pos = prep_data(parsed_dframe,timestamps=[buffer_timestamp,timestamp],tokenizer=tokenizer)
+    parsed_dframe = parsed_dframe.loc[parsed_dframe.timestamp > timestamp].reset_index()
+    return token_queries,token_neg,token_pos,parsed_dframe,most_recent_timestamp
 
 def load_model():
-    model = tf.keras.models.load_model('./models/dns_anhilator.h5')
+#    model = tf.keras.models.load_model('./models/dns_anhilator.h5')
+    model = tf.keras.models.load_model('./models/metric_model.h5')
     return model
 
 def online_learn(learner,ref,eps=0.1):
--- photon_sphere_online.py
+++ photon_sphere_online.py
@@ -24,8 +24,8 @@ if __name__=='__main__':
     max_timestamp = 1600397395 # arbitrary recent timestamp
 
     model = fn.load_model() #model that learns online
-    ref_model = tf.keras.models.clone_model(model) #reference model
-    ref_model.set_weights(model.get_weights())
+#    ref_model = tf.keras.models.clone_model(model) #reference model
+#    ref_model.set_weights(model.get_weights())
 
     model.optimizer.lr.assign(1e-4)
     df = fn.load_gravity(table='domainlist')
@@ -33,19 +33,19 @@ if __name__=='__main__':
     i = 0
     bad_domains_all = []
     while True:
-        tokens,masks,labels,parsed_df,max_timestamp = fn.run_all(tokenizer=tokenizer,timestamp=max_timestamp)
+        queries,neg_samples,anchor_samples,parsed_df,max_timestamp = fn.run_all(tokenizer=tokenizer,timestamp=max_timestamp)
         if i == 0:
             i =+ 1
             continue
         print('{0} pieces of matter entering the photon sphere..'.format(parsed_df.mask_count))
         print(tokens.shape)
-        pred_probs = model.predict([tokens,masks])
-        ref_pred_probs = ref_model.predict([tokens,masks])
-        predicted = np.where(pred_probs>0.5,1,0).astype(bool)
-        ref_predicted = np.where(ref_pred_probs>0.5,1,0).astype(bool)
-        
-        domain_lists = parsed_df.domain_list.split(',')
-        bad_domains = np.array(domain_lists)[predicted[0,:len(domain_lists)]]
+        pred_probs = model.predict([queries,neg_samples,anchor_samples])
+        #ref_pred_probs = ref_model.predict([tokens,masks])
+        #predicted = np.where(pred_probs>0.5,1,0).astype(bool)
+        #ref_predicted = np.where(ref_pred_probs>0.5,1,0).astype(bool)
+        bad_domains = parsed_df.loc[pred_probs[:,0]<pred_probs[:,1]].domain.values
+        #domain_lists = parsed_df.domain_list.split(',')
+        #bad_domains = np.array(domain_lists)[predicted[0,:len(domain_lists)]]
         #bad_domains = [i for i in bad_domains if len(i)>0]
         bad_domains = [i for i in bad_domains if i not in gravity and i not in df['domain'].values and i not in bad_domains_all]
         bad_domains_all.extend(bad_domains)
@@ -58,8 +58,9 @@ if __name__=='__main__':
             #bad_df = fn.create_dframe(bad_domains,max_timestamp)
             #df = pd.concat([df,bad_df])
             #fn.update_gravity(df)
-        online_labels = fn.online_learn(pred_probs,ref_pred_probs,eps=epsilon)
-        model.fit([tokens,masks],online_labels)
+
+            #        online_labels = fn.online_learn(pred_probs,ref_pred_probs,eps=epsilon)
+#        model.fit([tokens,masks],online_labels)
         time.sleep(2)
         i+=1
         if logging:
