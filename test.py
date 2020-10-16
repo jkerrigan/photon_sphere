c6ee3dced1028afdc6fe21174e96b79f667abbfd
Josh <josh@Joshs-MacBook-Pro.local>
2020-10-15 22:13:07 -0400

    update to test.py

--- test.py
+++ test.py
@@ -8,6 +8,6 @@ def test_mask():
 def test_data_prep():
     df = pd.DataFrame({'domain':'test.domain2 test.domain3','blocked_chain':'0 1','mask_count':2},index=[0])
     tok_model = fn.load_yttm()
-    tok_result = fn.prep_data(df,tokenizer=tok_model)
+    tok_result = fn.prep_data(df,[10000,10001],tokenizer=tok_model)
     print(tok_result)
     assert len(tok_result) > 0
