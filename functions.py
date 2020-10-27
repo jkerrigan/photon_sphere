c02a0a08af2b37e5f4a8b06e678a531654f0bee3
Josh <josh@Joshs-MacBook-Pro.local>
2020-10-26 21:32:54 -0400

    Updated functions and testing

--- functions.py
+++ functions.py
@@ -8,21 +8,14 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 import tensorflow
 import youtokentome as yttm
 
-def make_mask(x):
-    """Deprecated"""
-    y = np.zeros(100)
-    print(x)
-    y[:x] = 1
-    return y
-
-def make_multilabel(x):
-    """Deprecated"""
-    x_ = list(map(lambda xx: int(xx),x.split(' ')))
-    y = np.zeros(100)
-    y[range(len(x_))] = x_
-    return y
 
 def multi_pred(model,query,neg_list,anchor_pos_list,anchor_neg_list):
+    """
+    Needs refactoring(for handling triplet loss model) and additional 
+    analysis to determine stability in prediction, i.e. relative
+    distances in the learned metric space can have significant 
+    variances depending on the positive anchor chosen.
+    """
     predictions = model.predict([len(neg_list)*[query],neg_list,anchor_pos_list,anchor_neg_list])
     #mean_dist = predictions[:,0].mean()
     mean_rel = (predictions[:,0]>predictions[:,3]).mean()
@@ -47,6 +40,11 @@ def load_query_list(timestamp=None):
     return df
 
 def update_gravity(df):
+    """
+    Not advisable to use. Simultaneous writing to the sql db
+    causes corruption isses. Adding domains by command line
+    is recommended.
+    """
     db_connect = create_engine('sqlite:////etc/pihole/gravity.db', connect_args={'timeout': 15})
     connection = db_connect.raw_connection()
     df.to_sql('domainlist', con=connection,if_exists='replace',index=None)
@@ -58,38 +56,14 @@ def create_dframe(domains,timestamp):
                                   'date_modified':len(domains)*[timestamp],
                                   'comment':len(domains)*['Added by Photon Sphere.']})
     return new_dataframe
-    
-def load_tokenizer():
-    """Deprecated"""
-    with open('tokenizer.pickle', 'rb') as handle:
-        tokenizer = pickle.load(handle)
-    return tokenizer
-
-def load_sentpiece():
-    """Deprecated"""
-    serialized_model_proto = tensorflow.gfile.GFile('./sentpiece/m.model', 'rb').read()
-    sp = spm.SentencePieceProcessor()
-    sp.load_from_serialized_proto(serialized_model_proto)
-    return sp
 
 def load_yttm():
     model_path = './yttm_model/yttm_ads.model'
     bpe = yttm.BPE(model=model_path)
     return bpe
-        
-def parse_data(df):
-    """Deprecated due to no use."""
-    df_ = df.copy()
-    df_['domain'] = df_['domain'].apply(lambda x: x.replace('.',' ').replace('-',' '))
-#    format_df = df.groupby('timestamp').sum().reset_index()
-#    format_df['domain'] = df.groupby('timestamp')['domain'].apply(lambda x: '|'.join(x)).reset_index()['domain']
-#    format_df['domain_list'] = df.groupby('timestamp')['domain'].apply(lambda x: ','.join(x)).reset_index()['domain']
-#    format_df['mask_count'] = df.groupby('timestamp')['domain'].apply(lambda x: len(x)).reset_index()['domain']
-#    format_df['domain'] = format_df['domain'].apply(lambda x: ' '.join(x.split('.')))
-#    format_df['blocked_chain'] = df.groupby('timestamp')['blocked'].apply(lambda x: ' '.join([str(i) for i in x])).reset_index()['blocked']
-    return df_
 
 def prep_data(df,timestamps,tokenizer=None):
+    """Needs to be refactored for a triplet loss based model."""
     encoded_queries = pad_sequences(tokenizer.encode(list(df.loc[df.timestamp>timestamps[1]].domain.values), output_type=yttm.OutputType.ID),30,padding='post')
     df_neg = df.loc[(df.blocked==1)&(df.timestamp<timestamps[1])].domain.values
     df_pos_anchors = df.loc[(df.blocked==0)&(df.timestamp<timestamps[1])].domain.values
@@ -121,7 +95,6 @@ def run_all(tokenizer=None,timestamp=None):
         new_entries = np.sum(dframe.timestamp>timestamp)
         i+=1
     most_recent_timestamp = dframe.timestamp.iloc[-1]
-#    parsed_dframe = parse_data(dframe)
     token_queries,token_pos,token_neg,token_pos_anchor,token_neg_anchor = prep_data(dframe,timestamps=[buffer_timestamp,timestamp],tokenizer=tokenizer)
     dframe = dframe.loc[dframe.timestamp > timestamp].reset_index()
     return token_queries,token_pos,token_neg,token_pos_anchor,token_neg_anchor,dframe,most_recent_timestamp
@@ -141,6 +114,7 @@ def quadruplet_loss(true,pred):
     return loss
 
 def custom_acc(true,pred):
+    """Needs refactoring."""
     try:
         pred_compare = tf.where(pred[:,0] < pred[:,1],True,False) & tf.where(pred[:,2] < pred[:,1],True,False)
         total = tf.keras.backend.sum(tf.cast(pred_compare,tf.float32))
@@ -149,10 +123,12 @@ def custom_acc(true,pred):
         return 0
     
 def load_model():
+    """Needs refactoring."""
     model = tf.keras.models.load_model('./models/siamese_metric_quadruplet_loss_v1.h5',custom_objects={'quadruplet_loss':quadruplet_loss,'custom_acc':custom_acc})
     return model
 
 def online_learn(learner,ref,eps=0.1):
+    """Developmental."""
     ref_labels = np.where(ref > 0.5, 1, 0)
     print('Models diverge: {0}'.format(np.sum(learner_labels==ref_labels)/ref_labels.size < 1))
     if np.random.rand() > eps:
--- test.py
+++ test.py
@@ -1,13 +1,16 @@
 import functions as fn
 import pandas as pd
 
-def test_mask():
-    mask = fn.make_mask(10)
-    assert sum(mask) == 10 and len(mask)==100
-
 def test_data_prep():
+    """Needs to be rewritten."""
     df = pd.DataFrame({'domain':'test.domain2 test.domain3','blocked_chain':'0 1','mask_count':2},index=[0])
     tok_model = fn.load_yttm()
     tok_result = fn.prep_data(df,[10000,10001],tokenizer=tok_model)
     print(tok_result)
     assert len(tok_result) > 0
+
+def test_create_dframe():
+    domains = ['test.domain1','test.domain2','test.domain3','test.domain4']
+    timestamp = [0,1,2,3]
+    df = fn.create_dframe(domains,timestamp)
+    assert df['domain'].values=='test.domain1'
